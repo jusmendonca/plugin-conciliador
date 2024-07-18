e46febaa903e625072138505649b403c5c42969a
Igor Mendonça Cardoso Gomes <cadmendonca@hotmail.com>
2024-07-18 08:15:01 -0300

    Update
    Implemento de opções de cópia. A resolver: undefined.

--- popup.js
+++ popup.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const processButton = document.getElementById('processButton');
     const statusDiv = document.getElementById('status');
     const resultDiv = document.getElementById('result');
+    const copyOption = document.getElementById('copyOption');
 
     let selectedFile;
 
     fileInput.addEventListener('change', handleFileSelect);
     dibInput.addEventListener('input', handleInput);
     processButton.addEventListener('click', processFile);
+    copyOption.addEventListener('change', storeData);
 
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter' && !processButton.disabled) {
@@ -30,8 +32,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function handleInput() {
+        formatDibInput();
         storeData();
         updateButtonState();
+    }
+
+    function formatDibInput() {
+        let dib = dibInput.value.replace(/\D/g, '');
+        if (dib.length > 2) dib = dib.slice(0, 2) + '/' + dib.slice(2);
+        if (dib.length > 5) dib = dib.slice(0, 5) + '/' + dib.slice(5, 9);
+        dibInput.value = dib;
     }
 
     function updateButtonState() {
@@ -55,11 +65,29 @@ document.addEventListener('DOMContentLoaded', function() {
         const result = findDataByDib(data, dib);
 
         if (result) {
-            const fullText = formatResult(result, dib);
-            const conciseText = formatConciseResult(result, dib);
-            copyToClipboard(fullText);
-            showMessage("Parâmetros copiados com sucesso! \n\ Use Ctrl+V ou 'colar' para inseri-los no documento.");
-            displayResult(conciseText);
+            const selectedOption = copyOption.value;
+            let textToCopy = '';
+            let textToDisplay = '';
+            switch (selectedOption) {
+                case 'totalValue':
+                    textToCopy = `R$ ${formatCurrency(result.soma)}`;
+                    textToDisplay = `R$ ${formatCurrency(result.soma)}`;
+                    break;
+                case 'concise':
+                    textToCopy = formatConciseResult(result, dib);
+                    textToDisplay = textToCopy;
+                    break;
+                case 'full':
+                    textToCopy = formatResult(result, dib);
+                    textToDisplay = formatConciseResult(result, dib);
+                    break;
+                default:
+                    textToCopy = formatResult(result, dib);
+                    textToDisplay = textToCopy;
+            }
+            copyToClipboard(textToCopy);
+            showMessage("Valor(es) copiado(s) com sucesso! \nUse Ctrl+V ou 'colar' para inseri-los no documento.");
+            displayResult(textToDisplay);
         } else {
             showMessage("Nenhum resultado encontrado para a DIB especificada.");
             displayResult("");
@@ -100,56 +128,38 @@ document.addEventListener('DOMContentLoaded', function() {
     function formatResult(result, dib) {
         const { dip, p_ant, p_atual, v_ant, v_atual, soma } = result;
         return `
-
-
-
 DIB (=DER): ${formatDate(dib)}
 
 -----------------------------------------------------------------------------------------------------------------------------------------------
-
 
 DIP: ${formatDate(new Date(dip))}
 
 -----------------------------------------------------------------------------------------------------------------------------------------------
 
-
 RMI: 01 (um) salário-mínimo
 
 -----------------------------------------------------------------------------------------------------------------------------------------------
-
 
 VALOR TOTAL DO ACORDO: R$ ${formatCurrency(soma)}
 
 -----------------------------------------------------------------------------------------------------------------------------------------------
 
-
 COMPOSIÇÃO:
 
 - Parcelas de exercícios anteriores: ${p_ant}
-
 - Parcelas do exercício atual: ${p_atual}
-
 - Valor de exercícios anteriores: R$ ${formatCurrency(v_ant)}
-
 - Valor do exercício atual: R$ ${formatCurrency(v_atual)}
-
-
-
         `.trim();
     }
 
     function formatConciseResult(result, dib) {
         const { dip, p_ant, p_atual, v_ant, v_atual, soma } = result;
         return `
-    
 DIB: ${formatDate(dib)}
----------------------------------------------------------------
 DIP: ${formatDate(new Date(dip))}
----------------------------------------------------------------
 RMI: 01 (um) salário-mínimo
----------------------------------------------------------------
 VALOR TOTAL DO ACORDO: R$ ${formatCurrency(soma)}
----------------------------------------------------------------
 COMPOSIÇÃO:
 - Parcelas de exercícios anteriores: ${p_ant}
 - Parcelas do exercício atual: ${p_atual}
@@ -184,6 +194,7 @@ COMPOSIÇÃO:
 
     function storeData() {
         localStorage.setItem('dibInput', dibInput.value);
+        localStorage.setItem('copyOption', copyOption.value);
         if (selectedFile) {
             const reader = new FileReader();
             reader.onload = () => {
@@ -197,11 +208,16 @@ COMPOSIÇÃO:
 
     function loadStoredData() {
         const storedDib = localStorage.getItem('dibInput');
+        const storedCopyOption = localStorage.getItem('copyOption');
         const storedFile = localStorage.getItem('selectedFile');
         const storedFileName = localStorage.getItem('fileName');
 
         if (storedDib) {
             dibInput.value = storedDib;
+        }
+
+        if (storedCopyOption) {
+            copyOption.value = storedCopyOption;
         }
 
         if (storedFile && storedFileName) {
